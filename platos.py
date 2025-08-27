68c676ea92b75c1a2998ea47db74c3e4bf22c725
SergioD10 <sergiodiosa10@gmail.com>
2025-08-27 10:52:38 -0500

    Creacion platos.py y menu.py

---
+++ platos.py
@@ -0,0 +1,28 @@
+class comida:
+    def __init__(self, nombre:str, precio:float):
+        self.nombre = nombre
+        self.__precio = precio # atributo privado
+    def get_precio(self)->float:
+        return self.__precio   
+
+class platofuerte(comida):
+    def __init__(self, nombre:str, precio:float, ingredientes:list):
+        super().__init__(nombre, precio)
+        self.ingredientes = ingredientes
+    def mostrar(self):
+        return f"{self.nombre}\n Precio: ${self.get_precio()}\n Ingredientes: {', '.join(self.ingredientes)}"
+    
+class bebida(comida):
+    def __init__(self, nombre:str, precio:float, onzas:float):
+        super().__init__(nombre, precio)
+        self.onzas = onzas
+    def mostrar(self):
+        return f"{self.nombre}\n Precio: ${self.get_precio()}\n Onzas: {self.onzas} oz"
+
+class postre(comida):
+    def __init__(self, nombre:str, precio:float, sabor:str):
+        super().__init__(nombre, precio)
+        self.sabor = sabor
+    def mostrar(self):
+        return f"{self.nombre}\n Precio: ${self.get_precio()}\n Sabor: {self.sabor}"
+    
