99097e4504674dcb1fb5034980cd35db695983bf
GiovannyBM06 <giovannybm2006@gmail.com>
2025-08-30 08:42:41 -0500

    Creación del menú gráfico y ajuste del método ordenar

--- src/CUENTA.py
+++ src/CUENTA.py
@@ -1,26 +1,13 @@
 from MENU import platosfuertes, bebidas, postres
-
+from COMIDA import COMIDA
 class OPERACIONES:
 
     def __init__(self):
         self.platos = []
 
-    def ordenar (self, op)->str:
-        for plato in platosfuertes:
-            if plato.nombre == op:
-                self.platos.append(plato)
-                return f"Has ordenado: {plato.nombre } - ${plato.get_precio()}"   
-            
-        for bebida in bebidas:
-            if bebida.nombre == op:
-                self.platos.append(bebida)
-                return f"Has ordenado: {bebida.nombre } - ${bebida.get_precio()}"
-            
-        for postre in postres:
-            if postre.nombre == op:
-                self.platos.append(postre)
-                return f"Has ordenado: {postre.nombre}-${postre.get_precio()}"    
-        return "Plato no encontrado"    
+    def ordenar (self, item):
+        if isinstance(item,COMIDA):
+            self.platos.append(item)
 
     def total_pagar(self)-> float:
         total:float = sum(plato.get_precio() for plato in self.platos)
@@ -30,6 +17,8 @@ class OPERACIONES:
             print(f"{plato.nombre} - ${plato.get_precio()}")
         print("-"*30)
         return total
+    
+
     def propina(self, porcentaje:float)->str:
         total:float = sum(plato.get_precio() for plato in self.platos)
         propina =total*(porcentaje/100)
